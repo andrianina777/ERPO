c80924a576bc1e36d57a74ab516fafabe2f743b3
zorro <zorro@zorro.ovh>
2023-11-27 08:48:57 +0300

    Deploy simul

--- autobuild/build.sh
+++ autobuild/build.sh
@@ -29,10 +29,22 @@ mkdir -p "$TARGET_DIR/backend/"
 rsync -av --delete "target/prepa-V1.0.war" "$TARGET_DIR/backend/"
 cat << EOF > "$TARGET_DIR/backend/interfaces"
 OPHAM -> 192.168.130.2
-SIMUL221 -> 192.168.130.221
-SIMUL220 -> 192.168.130.220
 EOF
 
 echo "*** restart service ***"
 sudo systemctl restart prepa
+
+echo "*** build simul ***"
+sed -i "s/server.port=.*/server.port=12082/g" backend/prepa/target/classes/application.properties
+./mvnw package
+
+echo "*** deploy simul ***"
+mkdir -p "$TARGET_DIR/backend-simul/"
+rsync -av --delete "target/prepa-V1.0.war" "$TARGET_DIR/backend-simul/"
+cat << EOF > "$TARGET_DIR/backend-simul/interfaces"
+SIMUL220 -> 192.168.130.220
+EOF
+
+echo "*** restart simul service ***"
+sudo systemctl restart prepa-simul
 
